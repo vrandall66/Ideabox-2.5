703c3098db7f49d2dba8056230aefd8aae46cc2d
Vanessa Randall <vanessalrandall@gmail.com>
2019-09-25 19:06:41 -0600

    Add Ideas component file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React, { Component } from "react";
+import Ideas from "./Ideas";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      ideas: [
+        {
+          id: 1,
+          title: "Finish React pre-work",
+          description:
+            'Finish reading Tyler McGinnis" React tutorial, Asynchronous JS videos, and React Router reading'
+        },
+        {
+          id: 2,
+          title: "Meal prep for a week or two",
+          description:
+            "Thaw some chicken to slow cook. Make something with zucchini noodles and shrimp"
+        },
+        { id: 3, title: "Laundry", description: 'towels and regan"s stuff' }
+      ]
+    };
   }
 
   render() {
     return (
       <main className="App">
         <h1>IdeaBox</h1>
-        <p>Hi!</p>
+        <Ideas />
       </main>
     );
   }
