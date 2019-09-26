0d8e56fdb923da6792e18ea4389c3e187ea133e9
Vanessa Randall <vanessalrandall@gmail.com>
2019-09-25 18:54:36 -0600

    Wipe initial contents of App.js and define constructor and render method

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <main className="App">
+        <h1>IdeaBox</h1>
+        <p>Hi!</p>
+      </main>
+    );
+  }
 }
 
 export default App;
